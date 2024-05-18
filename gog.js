2e2094dc3e1562873b2692c1571079fda8c71dfa
Alessandro Pignotti <a.pignotti@sssup.it>
2024-05-18 20:24:29 +0200

    Progress on the UX

--- gog.js
+++ gog.js
@@ -1,49 +1,55 @@
-(async function(){
-	async function getGamesData()
+async function getGamesData()
+{
+	chrome.runtime.sendMessage({type: "enableTab"});
+	// TODO: Cache data and only update new entries in licenses
+	try
 	{
-		chrome.runtime.sendMessage({type: "enableTab"});
-		// TODO: Cache data and only update new entries in licenses
-		var supportedGames = [];
-		try
+		// Get the list of all purchased games
+		var r = await fetch("https://menu.gog.com/v1/account/licences", { credentials: "include" });
+		var licenses = await r.json();
+		for(var i=0;i<licenses.length;i++)
 		{
-			// Get the list of all purchased games
-			var r = await fetch("https://menu.gog.com/v1/account/licences", { credentials: "include" });
-			var licenses = await r.json();
-			for(var i=0;i<licenses.length;i++)
-			{
-				var id = licenses[i];
-				var r = await fetch(`https://api.gog.com/v2/games/${id}`);
-				var gameData = await r.json();
-				// Only consider DOSBox games for now
-				if(!gameData.isUsingDosBox)
-					continue;
-				// Save the information we need for this game
-				var title = gameData._embedded.product.title;
-				var imgUrl = gameData._embedded.product._links.image.href.replace("{formatter}", "glx_logo_2x");
-				supportedGames.push({id, title, imgUrl});
-			}
+			var id = licenses[i];
+			var r = await fetch(`https://api.gog.com/v2/games/${id}`);
+			var gameData = await r.json();
+			// Only consider DOSBox games for now
+			if(!gameData.isUsingDosBox)
+				continue;
+			// Save the information we need for this game
+			var title = gameData._embedded.product.title;
+			var imgUrl = gameData._embedded.product._links.image.href.replace("{formatter}", "glx_logo_2x");
+			gamesList.appendChild(createGameDiv(id, title, imgUrl));
 		}
-		catch(e)
-		{
-			debugger;
-		}
-		return supportedGames;
 	}
-	function createGameDiv(gameData)
+	catch(e)
 	{
-		var d = document.createElement("div");
-		var i = document.createElement("img");
-		i.src = gameData.imgUrl;
-		d.appendChild(i);
-		var t = document.createElement("span");
-		t.textContent = gameData.title;
-		d.appendChild(t);
-		return d;
+		debugger;
 	}
-	var supportedGames = await getGamesData();
+}
+async function handleGameStart(ev)
+{
+	var id = ev.currentTarget.getAttribute("data-id");
+	var r = await fetch(`https://www.gog.com/account/gameDetails/${id}.json`);
+	var d = await r.json();
+	// TODO: Download Windows installer
+}
+function createGameDiv(id, title, imgUrl)
+{
+	var d = document.createElement("div");
+	d.setAttribute("data-id", id);
+	d.addEventListener("click", handleGameStart);
+	var i = document.createElement("img");
+	i.src = imgUrl;
+	d.appendChild(i);
+	var t = document.createElement("span");
+	t.textContent = title;
+	d.appendChild(t);
+	return d;
+}
+async function init(){
+	var gamesList = document.getElementById("gamesList");
+	await getGamesData();
 	var loading = document.getElementById("gamesLoading");
 	loading.style.display = "none";
-	var gamesList = document.getElementById("gamesList");
-	for(var i=0;i<supportedGames.length;i++)
-		gamesList.appendChild(createGameDiv(supportedGames[i]));
-})()
+}
+document.addEventListener("DOMContentLoaded", init);
