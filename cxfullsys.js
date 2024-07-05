8302ee2efc2cf92993280d5d70a72e16f8eb47ad
Alessandro Pignotti <a.pignotti@sssup.it>
2024-07-05 09:36:11 +0200

    Add full system environment

---
+++ cxfullsys.js
@@ -0,0 +1,24 @@
+var port = null;
+var cx = null;
+async function handleMessage(m)
+{
+	var data = m.data;
+	if(data.type == "port")
+	{
+		port = data.port;
+		port.onmessage = handleMessage;
+		await CheerpXSystem.promise;
+		cx = await CheerpXSystem.create();
+		port.postMessage({type: "response", responseId: data.responseId, value: null});
+	}
+	else if(data.type == "start")
+	{
+		var gameConfig = data.gameConfig;
+		cx.run(/*MhZ*/20, {mem:64, bios:"/files/bios.bin", vgaBios:"/files/vgabios-stdvga.bin", disk:[gameConfig.dosImage, gameConfig.gameImage]});
+	}
+	else
+	{
+		debugger;
+	}
+}
+addEventListener("message", handleMessage);
