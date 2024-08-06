b3690241e881a34904435327a45b9bf14ee4d710
Alessandro Pignotti <a.pignotti@sssup.it>
2024-08-06 18:54:38 +0200

    UX bump
    * Show both supported and unsupported games
    * Show direct link to free games
    * Unify handling of stores and games list

--- gog.js
+++ gog.js
@@ -6,6 +6,45 @@ cxLinuxPort.onmessage = cxMsg;
 var cxFullSysChannel = new MessageChannel();
 var cxFullSysPort = cxFullSysChannel.port1;
 cxFullSysPort.onmessage = cxMsg;
+var supportedGamesList = null;
+var freeGamesList = null;
+var unsupportedGamesList = null;
+function GamesList(parent, title, tooltip, isClickable)
+{
+	this.listDiv = document.createElement("div");
+	this.listDiv.classList.add("itemList");
+	// Keep the list hidden at first, we want to only display it
+	// when the first element is added
+	this.listDiv.classList.add("hidden");
+	this.listDiv.title = tooltip;
+	var titleElem = document.createElement("h2");
+	titleElem.textContent = title;
+	this.listDiv.appendChild(titleElem);
+	this.isClickable = isClickable;
+	parent.appendChild(this.listDiv);
+}
+function createGameDiv(id, title, imgUrl)
+{
+	var d = document.createElement("div");
+	d.setAttribute("data-id", id);
+	d.classList.add("game");
+	d.classList.add("clickable");
+	if(this.isClickable)
+		d.classList.add("clickable");
+	else
+		d.classList.add("grayed");
+	var i = document.createElement("img");
+	// Add a prefix to allow interception by service worker
+	i.src = "@" + imgUrl;
+	d.appendChild(i);
+	var t = document.createElement("span");
+	t.textContent = title;
+	d.appendChild(t);
+	this.listDiv.appendChild(d);
+	// Since we are adding an element, make the list visible
+	this.listDiv.classList.remove("hidden");
+}
+GamesList.prototype.addGame = createGameDiv;
 function cxMsg(m)
 {
 	var data = m.data;
@@ -43,9 +82,8 @@ async function sendMessageAndWaitReply(port, msg)
 		port.postMessage(msg);
 	});
 }
-async function getGamesData()
+async function getGamesData(gamesList)
 {
-	var gamesList = document.getElementById("gamesList");
 	// TODO: Cache data and only update new entries in licences
 	try
 	{
@@ -53,19 +91,31 @@ async function getGamesData()
 		var r = await fetch("https://menu.gog.com/v1/account/licences", { credentials: "include" });
 		if(r.status == 401)
 			return false;
+		gamesList.classList.remove("hidden");
 		var licenses = await r.json();
 		for(var i=0;i<licenses.length;i++)
 		{
 			var id = licenses[i];
 			var r = await fetch(`https://api.gog.com/v2/games/${id}`);
-			var gameData = await r.json();
-			// Only consider DOSBox games for now
-			if(!gameData.isUsingDosBox)
+			if(r.status != 200)
 				continue;
-			// Save the information we need for this game
-			var title = gameData._embedded.product.title;
-			var imgUrl = gameData._embedded.product._links.image.href.replace("{formatter}", "glx_logo_2x");
-			gamesList.appendChild(createGameDiv(id, title, imgUrl));
+			try
+			{
+				var gameData = await r.json();
+				// Save the information we need for this game
+				var title = gameData._embedded.product.title;
+				var imgUrl = gameData._embedded.product._links.image.href.replace("{formatter}", "glx_logo_2x");
+				// Only DosBOX games are supported for now
+				if(gameData.isUsingDosBox)
+					supportedGamesList.addGame(id, title, imgUrl);
+				else
+					unsupportedGamesList.addGame(id, title, imgUrl);
+			}
+			catch(e)
+			{
+				// Be robust to unexpected formats
+				console.warn(`Cannot parse data for game ${id}`);
+			}
 		}
 	}
 	catch(e)
@@ -102,23 +152,10 @@ async function handleGameStart(ev)
 	await sendMessageAndWaitReply(cxFullSysPort, {type: "start", gameConfig: gameConfig});
 debugger;
 }
-function handleStoreLogin(ev)
+function handleLinkElement(ev)
 {
-	var link = getAttributeFromAncestor(ev.target, "data-link");
+	var link = getAttributeFromAncestor(ev.target, "data-id");
 	window.open(link, "_blank");
-}
-function createGameDiv(id, title, imgUrl)
-{
-	var d = document.createElement("div");
-	d.setAttribute("data-id", id);
-	var i = document.createElement("img");
-	// Add a prefix to allow interception by service worker
-	i.src = "@" + imgUrl;
-	d.appendChild(i);
-	var t = document.createElement("span");
-	t.textContent = title;
-	d.appendChild(t);
-	return d;
 }
 async function initCheerpXLinux()
 {
@@ -151,20 +188,31 @@ async function initCheerpXFullSys()
 	});
 }
 async function init(){
+	var gamesList = document.getElementById("gamesList");
+	// The list of supported games, clickable
+	supportedGamesList = new GamesList(gamesList, "Supported games", "Select a game to play", /*isClickable*/true);
+	// Direct link to the free games selection for supported stores
+	freeGamesList = new GamesList(gamesList, "Get more games", "Visit store", /*isClickable*/true);
+	freeGamesList.addGame("https://www.gog.com/en/games?priceRange=0,0&hideDLCs=true&releaseDateRange=1980,1999", "Free games on GOG.com", "gogassets/logo.png");
+	// The list of unsupported games, not clickable and grayed out
+	unsupportedGamesList = new GamesList(gamesList, "Unsupported games", "Not currently supported", /*isClickable*/false);
 	var statusMessage = document.getElementById("statusMessage");
 	statusMessage.textContent = "Loading games";
-	var gamesList = document.getElementById("gamesList");
-	gamesList.addEventListener("click", handleGameStart);
-	var storeList = document.getElementById("storeList");
-	storeList.addEventListener("click", handleStoreLogin);
-	var hasGamesData = await getGamesData();
+	supportedGamesList.listDiv.addEventListener("click", handleGameStart);
+	freeGamesList.listDiv.addEventListener("click", handleLinkElement);
+	var hasGamesData = await getGamesData(gamesList);
 	if(hasGamesData)
+	{
 		statusMessage.textContent = "Click on a game to play";
+	}
 	else
 	{
+		var storeList = document.getElementById("storeList");
+		var supportedStoreList = new GamesList(storeList, "Supported stores", "Select a store to login", /*isClickable*/true);
+		supportedStoreList.addGame("https://www.gog.com/en/##openlogin", "Login to GOG.com", "gogassets/logo.png");
+		supportedStoreList.listDiv.addEventListener("click", handleLinkElement);
 		statusMessage.textContent = "Reload page after logging in";
 		storeList.classList.remove("hidden");
-		gamesList.classList.add("hidden");
 	}
 	var loading = document.getElementById("spinner");
 	loading.style.display = "none";
